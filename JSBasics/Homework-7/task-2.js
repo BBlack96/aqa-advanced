b83f8d0986cb17273d79fe028a55a574172bb03f
admin <bogdan7450@gmail.com>
2024-02-06 18:36:00 +0200

    Homework-7 fix accrording to the comment

--- JSBasics/Homework-7/task-2.js
+++ JSBasics/Homework-7/task-2.js
@@ -1,4 +1,4 @@
 const numbers = [1, 2, 3, 4, 5]
-const multipliedNumbers = numbers.map((index, value) => index *= value)
+const multipliedNumbers = numbers.map((value, index) => value *= index)
 
 console.log(multipliedNumbers)
