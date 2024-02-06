f53908691e22fee7c8cf8a6125fe868af8fd53e0
admin <bogdan7450@gmail.com>
2024-02-06 16:18:16 +0200

    Homework-7

---
+++ JSBasics/Homework-7/task-2.js
@@ -0,0 +1,4 @@
+const numbers = [1, 2, 3, 4, 5]
+const multipliedNumbers = numbers.map((acc, value) => acc *= value)
+
+console.log(multipliedNumbers)
---
+++ JSBasics/Homework-7/task-5.js
@@ -0,0 +1,5 @@
+const firstArray = [2, 4, 6, 8, 10]
+const secondArray = [10, 20, 30, 40, 50]
+const uniteArray = firstArray.concat(secondArray)
+
+console.log(uniteArray)
