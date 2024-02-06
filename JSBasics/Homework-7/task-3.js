93f1c16803fbffb40de8ecffe627a4af31a73453
admin <bogdan7450@gmail.com>
2024-02-06 15:37:22 +0200

    Homework 7

---
+++ JSBasics/Homework-7/task-3.js
@@ -0,0 +1,5 @@
+const numbers = [10, 20, 30, 40, 50]
+
+const sumNumbers = numbers.reduce((acc, sum) => acc += sum)
+
+console.log(sumNumbers)
---
+++ JSBasics/Homework-7/task-5.js
@@ -0,0 +1,5 @@
+const firstArray = [10, 20, 30, 40, 50]
+const secondArray = [ 0, 2, 6, 12, 20]
+const uniteArray = secondArray.concat(firstArray)
+
+console.log(uniteArray)
